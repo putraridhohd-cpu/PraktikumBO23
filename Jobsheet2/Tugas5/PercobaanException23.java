fd13583364b7a05be72775e85991c9d16ca16427
valentridho <putraridhohd@gmail.com>
2026-09-06 09:45:19 +0700

    Tugas praktikum 5 bagian A

---
+++ Jobsheet2/Tugas5/PercobaanException23.java
@@ -0,0 +1,11 @@
+package Jobsheet2.Tugas5;
+
+public class PercobaanException23 {
+    public static void main(String[] args) {
+        int[] angka = {1, 2, 3};
+
+        System.out.println("Awal Program");
+        System.out.println(angka[3]);
+        System.out.println("Akhir Program");
+    }
+}
