08da79caf228171a4ef3f071a49de23317441acc
valentridho <putraridhohd@gmail.com>
2026-09-10 08:32:10 +0700

    Percobaan 2 - Access Modifier

--- MotorEncapsulation/Motor23.java
+++ MotorEncapsulation/Motor23.java
@@ -1,17 +1,48 @@
 package MotorEncapsulation;
 
 public class Motor23 {
-    public int kecepatan = 0;
-    public boolean kontak0n = false;
+    private int kecepatan = 0;
+    private boolean kontak0n = false;
 
-    public void printStatus() {
+    public void nyalakanMesin() {
+        kontak0n = true;
+    }
+
+    public void matikanMesin() {
+        kontak0n = false;
+        kecepatan = 0;
+    }
+
+    public void tambahKecepatan() {
+        if (kontak0n == true) {
+            kecepatan += 5;
+        }
+        else {
+            System.out.println("Kecepatan motor valent tidak bisa nambah karena mesin nya off! \n ");
+        }
+    }
+    
+    public void kurangiKecepatan() {
+        if (kontak0n = true) {
+            kecepatan -= 5;
+    }
+    else {
+        System.out.println("kecepatan motor valent  tidak bisa berurang karena mesin OFF! \n");
+    }
+
+    
+
+    }
+     public void printStatus() {
         if (kontak0n == true) {
             System.out.println("Kontak 0n");
         }
         else {
             System.out.println("Kontak 0ff");
         }
-        System.out.println("kecepatan = " + kecepatan + "\n");
+        System.out.println("kecepatan motor valent = " + kecepatan + "\n");
     }
+
+    
     
 }
--- MotorEncapsulation/MotorDemo23.java
+++ MotorEncapsulation/MotorDemo23.java
@@ -4,7 +4,21 @@ public class MotorDemo23 {
     public static void main(String[] args) {
         Motor23 motor = new Motor23();
         motor.printStatus();
-        motor.kecepatan = 50;
+        motor.tambahKecepatan();
+
+        motor.nyalakanMesin();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.matikanMesin();
         motor.printStatus();
     }
     
