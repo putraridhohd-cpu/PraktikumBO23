53cb325ad0d6ac5ca6730dd776766efd67cd6ace
valentridho <putraridhohd@gmail.com>
2026-09-10 09:19:33 +0700

    Modifikasi batasi kecepatan

--- MotorEncapsulation/Motor23.java
+++ MotorEncapsulation/Motor23.java
@@ -2,47 +2,48 @@ package MotorEncapsulation;
 
 public class Motor23 {
     private int kecepatan = 0;
-    private boolean kontak0n = false;
+    private boolean kontakOn = false;
 
     public void nyalakanMesin() {
-        kontak0n = true;
+        kontakOn = true;
     }
 
     public void matikanMesin() {
-        kontak0n = false;
+        kontakOn = false;
         kecepatan = 0;
     }
 
     public void tambahKecepatan() {
-        if (kontak0n == true) {
-            kecepatan += 5;
-        }
-        else {
-            System.out.println("Kecepatan motor valent tidak bisa nambah karena mesin nya off! \n ");
+        if (kontakOn == true) {
+            if (kecepatan + 5 <= 100) { // diset kecepatan nya ke 99!
+                kecepatan += 5;
+            } else {
+                kecepatan = 100;
+                System.out.println("Kecepatan mencapai batas maksimal (100 km/jam)! \n");
+            }
+        } else {
+            System.out.println("Kecepatan motor valent tidak bisa nambah karena mesin nya off! \n");
         }
     }
-    
+
     public void kurangiKecepatan() {
-        if (kontak0n = true) {
-            kecepatan -= 5;
-    }
-    else {
-        System.out.println("kecepatan motor valent  tidak bisa berurang karena mesin OFF! \n");
-    }
-
-    
-
-    }
-     public void printStatus() {
-        if (kontak0n == true) {
-            System.out.println("Kontak 0n");
+        if (kontakOn == true) { 
+            if (kecepatan - 5 >= 0) {
+                kecepatan -= 5;
+            } else {
+                kecepatan = 0;
+            }
+        } else {
+            System.out.println("Kecepatan motor valent tidak bisa berkurang karena mesin OFF! \n");
         }
-        else {
-            System.out.println("Kontak 0ff");
-        }
-        System.out.println("kecepatan motor valent = " + kecepatan + "\n");
     }
 
-    
-    
+    public void printStatus() {
+        if (kontakOn == true) {
+            System.out.println("Kontak On");
+        } else {
+            System.out.println("Kontak Off");
+        }
+        System.out.println("Kecepatan motor valent = " + kecepatan + "\n");
+    }
 }
