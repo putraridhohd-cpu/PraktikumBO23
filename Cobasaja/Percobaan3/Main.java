3ed92e6625458a62c6cdf11321fb39fc1aa343c3
valentridho <putraridhohd@gmail.com>
2026-09-18 10:26:03 +0700

    ubah nama

--- Cobasaja/Percobaan3/Main.java
+++ Cobasaja/Percobaan3/Main.java
@@ -1,10 +1,8 @@
 package Cobasaja.Percobaan3;
-
-// File: Main.java
 public class Main {
     public static void main(String[] args) {
-        Pegawai23 masinis = new Pegawai23("1234", "Setyo");
-        Pegawai23 asisten = new Pegawai23("5678", "Rahmad");
+        Pegawai23 masinis = new Pegawai23("1234", "Valent Ridho");
+        Pegawai23 asisten = new Pegawai23("5678", "Putra santoso");
         
         KeretaApi keretaApi = new KeretaApi("Gaya Baru", "Bisnis", masinis, asisten);
         
