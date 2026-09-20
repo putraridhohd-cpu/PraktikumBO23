2a431bea005fb34e012beb68ff63f43aeaab8037
valentridho <putraridhohd@gmail.com>
2026-09-20 11:11:12 +0700

    perbaikan error dan penambahan pada percobaan 1

--- Jobsheet4/Cobasaja/Percobaan1/Main.java
+++ Jobsheet4/Cobasaja/Percobaan1/Main.java
@@ -6,5 +6,18 @@ public class Main {
         Laptop23 l = new Laptop23("Asus", p);
         
         l.info();
+
+        Processor23 p1 = new Processor23();
+        p1.setMerk("infinix hot 40i");
+        p1.setCache(4);
+        Laptop23 l1 = new Laptop23();
+        l1.setMerk("Zyrex D tech pro V1");
+        l1.setProc(p1);
+        l1.info();
+
+        Laptop23 l2 = new Laptop23("Thinkpad", new Processor23("intel i5", 3));
+        l2.info();
+
+
     }
 }
